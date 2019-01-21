72d5247a12e4af6cf37c2340d29a2002b6bdd3bb
Paul Spence <hello@paulspence.me.uk>
2019-01-21 16:38:03 +0000

    added scroll to enter

--- templates/content-parts/header/content-home-header.php
+++ templates/content-parts/header/content-home-header.php
@@ -1,5 +1,8 @@
 <header id="home-header" class="large food">
     <img id="home-logo" src="<?php echo site_url('/wp-content/themes/intimation-pro/assets/img/Redemption-master-logo-white.png'); ?>" alt="Logo" srcset="">
+    <div class="scroll">
+        Scroll Down to Enter<br><i class="fa fa-chevron-down"></i>
+    </div>
 </header>
 
 <main>
