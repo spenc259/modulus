2c2dc0a4db27b20c9d766d61d26556cfeb2c24c9
Paul Spence <hello@paulspence.me.uk>
2019-01-21 14:14:48 +0000

    Added link to slides

--- templates/modules/slider.php
+++ templates/modules/slider.php
@@ -1,11 +1,16 @@
 
 <section class='col-12 slider'>
     <?php foreach ( $module['slider'] as $key => $slide ) : ?>
+        <?php $link = get_the_permalink($slide->ID); ?>
         <?php $image_id = get_the_post_thumbnail_url($slide->ID); ?>
-        <div class="slide">
-            <div class="slide-item" style="background-image: url('<?php echo $image_id; ?>');">
-                <div class="slide-inner"></div>
+        
+            <div class="slide">
+                <a href="<?php echo $link; ?>">
+                    <div class="slide-item" style="background-image: url('<?php echo $image_id; ?>');">
+                        <div class="slide-inner"></div>
+                    </div>
+                </a>
             </div>
-        </div>
+        
     <?php endforeach; ?>
 </section>
